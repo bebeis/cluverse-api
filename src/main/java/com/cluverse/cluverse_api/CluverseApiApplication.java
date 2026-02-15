40bd2a5e50b5fe8cfab63f583e74c0bd3d94d0fe
Junseo Bae <ferrater1013@gmail.com>
2026-02-16 03:59:31 +0900

    init: dependency 초기 설정

---
+++ src/main/java/com/cluverse/cluverse_api/CluverseApiApplication.java
@@ -0,0 +1,13 @@
+package com.cluverse.cluverse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluverseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cluverse/cluverse_api/CluverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluverse.cluverse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
